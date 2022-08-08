e918b3647a84ebdcf874de642c7e78761adf2485
Matt <mbrowne108@gmail.com>
2022-08-08 18:51:10 -0400

    Add sass for customization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import 'react-router-dom'
 import { Route, Routes } from 'react-router-dom';
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/main.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css';
 
