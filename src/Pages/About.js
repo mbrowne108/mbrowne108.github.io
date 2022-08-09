03efe862e14175ff9e1dd24cf6dbc0b6bf29cf73
Matt <mbrowne108@gmail.com>
2022-08-09 13:19:47 -0400

    Add about page content

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
+    <>
+      <div className='container border p-5 bg-light'>
         <h1>About Me</h1>
-    </div>
+        <p>Hello! My name is Matt Browne and I am a web developer, software engineer, and freelance classical composer living in New York, NY. My experience draws on fifteen years of music education, community outreach, collaboration, and creating music. I hold a certificate in Full Stack Web Development from the Flatiron School as well as a Doctorate in Music Composition from the University of Michigan. From my breadth of diverse experience, I bring creativity, a collaborative spirit, and rigid attention to detail to any project.</p>
+      </div>
+      <div className='container p-5'>
+        <h3>Technical Skills</h3>
+        <h5>JavaScript, React, Ruby on Rails, HTML, CSS, Bootstrap, SQL, Postgres, Sinatra</h5>
+      </div>
+    </>
   )
 }
 
