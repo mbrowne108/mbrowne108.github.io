1a78e0222fabf7063ec473e8e8aec1add5966512
Matt <mbrowne108@gmail.com>
2022-08-09 14:03:34 -0400

    Add links to all app demos and repos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import NavBar from './Components/NavBar.js'
 import Portfolio from './Pages/Portfolio.js'
 import About from './Pages/About.js'
 
+import linkedin from './Images/linkedin.png'
+import github from './Images/GitHub_Logo.png'
 
 function App() {
   return (
@@ -14,6 +16,15 @@ function App() {
         <Route path="/" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
+      <nav className='navbar bg-primary p-4 navbar-dark fixed-bottom'>
+        <h5 className='text-white'>Connect with me!</h5>
+        <a href="https://www.linkedin.com/in/matt-browne108/" className="mx-auto d-block nav" target="_blank">
+          <img src={linkedin}  height="50" className='nav-item'/>
+        </a>
+        <a href="https://github.com/mbrowne108/" className="mx-auto d-block nav" target="_blank">
+          <img src={github} height="50" className='nav-item'/>
+        </a> 
+      </nav>
     </div>
   );
 }
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,8 +3,8 @@ import React from 'react'
 function About() {
   return (
     <>
-      <div className='container border p-5 bg-light'>
-        <h1>About Me</h1>
+      <h1 className='text-center mt-3'>About Me</h1>
+      <div className='container border p-3 bg-light'>  
         <p>Hello! My name is Matt Browne and I am a web developer, software engineer, and freelance classical composer living in New York, NY. My experience draws on fifteen years of music education, community outreach, collaboration, and creating music. I hold a certificate in Full Stack Web Development from the Flatiron School as well as a Doctorate in Music Composition from the University of Michigan. From my breadth of diverse experience, I bring creativity, a collaborative spirit, and rigid attention to detail to any project.</p>
       </div>
       <div className='container p-5'>
